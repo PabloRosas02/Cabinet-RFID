08de6102f24b39820f0c6fa47f66c72053193763
Pablo <rosas.pablo@uabc.edu.mx>
2026-08-03 18:47:52 -0700

    .

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,5 +1,7 @@
 import app from '../backend/index.js';
 
 export default async function handler(req, res) {
+  const appModule = await import('../backend/index.js');
+  const app = appModule.default;
   return app(req, res);
 }
