6ea348859e22d6e89b4776bc45938a7b35d8f84b
Pablo <rosas.pablo@uabc.edu.mx>
2026-08-03 18:50:44 -0700

    ES Modules nativos

--- Server/api/index.mjs
+++ Server/api/index.mjs
@@ -1,7 +1,5 @@
 import app from '../backend/index.js';
 
 export default async function handler(req, res) {
-  const appModule = await import('../backend/index.js');
-  const app = appModule.default;
   return app(req, res);
 }
